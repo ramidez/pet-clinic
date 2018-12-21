b65ad5c6cfddd1378d52253bc5d9364fc5369c0d
Rafał Kałuski <rafal.kaluski@itwl.pl>
2018-12-21 12:26:40 +0100

    add POJOS. Closes #2.

---
+++ src/main/java/com/springframework/petclinic/model/Person.java
@@ -0,0 +1,23 @@
+package com.springframework.petclinic.model;
+
+public class Person {
+
+    private String firstName;
+    private String lasName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLasName() {
+        return lasName;
+    }
+
+    public void setLasName(String lasName) {
+        this.lasName = lasName;
+    }
+}
