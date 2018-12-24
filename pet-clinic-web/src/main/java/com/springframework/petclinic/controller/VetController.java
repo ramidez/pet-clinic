62d9217059041795de41f4ccde01a80c86fea295
Rafał Kałuski <rafal.kaluski@itwl.pl>
2018-12-24 11:28:25 +0100

    add index to owner and vet

---
+++ pet-clinic-web/src/main/java/com/springframework/petclinic/controller/VetController.java
@@ -0,0 +1,15 @@
+package com.springframework.petclinic.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/vet")
+@Controller
+public class VetController {
+
+    @RequestMapping({"/", "", "/index", "/index.html"})
+    public String listVets()
+    {
+        return "vet/index";
+    }
+}
