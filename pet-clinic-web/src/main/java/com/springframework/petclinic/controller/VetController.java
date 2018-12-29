041d9f370217affaa0d44d6768e6ee7680d86b0c
ramidez <1aezakmi1>
2018-12-29 21:51:49 +0100

    Add vet and owner list table

--- pet-clinic-web/src/main/java/com/springframework/petclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/com/springframework/petclinic/controller/VetController.java
@@ -1,15 +1,24 @@
 package com.springframework.petclinic.controller;
 
+import com.springframework.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/vet")
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/", "", "/index", "/index.html"})
-    public String listVets()
+    public String listVets(Model model)
     {
+        model.addAttribute("vets", vetService.findAll());
         return "vet/index";
     }
 }
